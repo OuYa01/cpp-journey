4497cd520e6993fff66c96d9d9b3c8d895af2c8e
OuYa1 <laamri2002@gmail.com>
2025-02-12 18:50:56 +0100

    i fix small error

--- level3/02_SumEachRowInMatrix.cpp
+++ level3/02_SumEachRowInMatrix.cpp
@@ -8,11 +8,11 @@ void PrintEachRowSumInMatrix(int M[3][3], int size)
 	int i, j;
 	int sum;
 
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
 		sum = 0;
 
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
 			sum += M[i][j];
 		}
