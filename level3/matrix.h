70ebfedee8efdf6d8d0d67ffe26548f0dd76db31
OuYa1 <laamri2002@gmail.com>
2025-02-13 22:52:01 +0100

    I add a prototype of function

--- level3/matrix.h
+++ level3/matrix.h
@@ -1,11 +1,12 @@
 #ifndef MATRIX_H
-#define MTARIX_H
+#define MATRIX_H
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
+
 void PrintMatrice(int M[3][3], short size);
 void FillMatrixWithRandomNumbers(int M[3][3], short size);
 int RandomNumber(int from, int to);
@@ -18,4 +19,8 @@ bool IsIdentityMatrix(int Matrix1[3][3], short Rows, short Cols);
 bool IsScalarMatrix(int Matrix1[3][3], short Rows, short Cols);
 int MaxNumberInMatrix(int Matrix1[3][3], short Rows, short Cols);
 int MinNumberInMatrix(int Matrix1[3][3], short Rows, short Cols);
+bool IsPalindromeMatrix(int M[3][3], short Rows, short Cols);
+bool IsPalindromeArray(int A[], short size);
+
+
 #endif
