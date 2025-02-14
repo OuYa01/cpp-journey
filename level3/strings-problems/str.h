e8738c1f569e564fa49e03c6be2d9678ca1a70db
OuYa1 <laamri2002@gmail.com>
2025-02-14 21:27:53 +0100

    this is the header file

---
+++ level3/strings-problems/str.h
@@ -0,0 +1,18 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void PrintFibonacciSequencerR(int length, int x = 0, int y = 1);
+void PrintFirstLetter(string str);
+string UpperCaseFirstLetter(string str);
+string LowerCaseFirstLetter(string str);
+string LowerAllString(string str);
+string UpperAllString(string str);
+char InvertLetterCase(char char1);
+
+#endif
