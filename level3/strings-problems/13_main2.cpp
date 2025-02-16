9b5bdc54ff35fcdbbc040ffb7b55b7e1eaa1d35b
OuYa1 <laamri2002@gmail.com>
2025-02-16 13:04:49 +0100

    i add another tests

--- level3/strings-problems/13_main2.cpp
+++ level3/strings-problems/13_main2.cpp
@@ -7,12 +7,32 @@
  */
 int main()
 {
+	//test 1
+	vector<string> v = splitStringIntoVector("It's Alwyas Seems Impossible Until It's Done", " ");
+	short tokens = 	v.size();
 
-	short tokens;
-	vector<string> v= splitStringIntoVector("It's Alwyas Seems Impossible Until It's Done", tokens);
-	
 	cout << "Tokens = " << tokens << endl;
-	for (string i : v)
+	for (string& i : v)
+		cout << i << endl;
+
+	cout << endl;
+	
+	//test 2
+	v = splitStringIntoVector("We,Do,Hard,Things", ",");
+	tokens = v.size();
+
+	cout << "Tokens = " << tokens << endl;
+	for (string& i : v)
+		cout << i << endl;
+
+	cout << endl;
+	
+	//test 3
+	v = splitStringIntoVector("Ouya#,,#01#,,#01", "#,,#");
+	tokens = v.size();
+
+	cout << "Tokens = " << tokens << endl;
+	for (string& i : v)
 		cout << i << endl;
 
 	return (0);
