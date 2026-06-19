996ec50ad091fe7a462def2a801543b5224a1233
OuYa01 <Laamri2002@gmail.com>
2026-06-19 16:59:13 +0100

    add missing libraries + I replace Login function from User_op file to main file

--- projects/3_BankSysteme1/main.cpp
+++ projects/3_BankSysteme1/main.cpp
@@ -1,5 +1,31 @@
-#include "main.h"
-#include "Transactions.h"
+#include "Headers/main.h"
+#include "Headers/users.h"
+#include "Headers/Transactions.h"
+
+
+void Login()
+{
+    vector<stUsers> AllUsersData;
+    LoadAllUsersDataFromFile(AllUsersData);
+    ShowLoginScreen();
+    stUsers RootUser;
+start:    
+    GetUserInfos(RootUser);
+
+    if (CheckIfUserExist(RootUser, AllUsersData))
+    {
+        vector<stClient> AllClientsData;
+        LoadAllClientsDataFromFile(AllClientsData);
+        MainMenueSwitch(AllClientsData, AllUsersData, RootUser);
+    }
+    else
+    {
+        cout << "Invalide UserName/Password!\n";
+        goto start;
+    }
+}
+
+
 
 /**
  * main - Program Entry
@@ -8,9 +34,7 @@
  */
 int main(void)
 {
-    // vector<stClient> AllClientsData;
-    // LoadAllClientsDataFromFile(AllClientsData);
-    // MainMenueSwitch(AllClientsData);
+
     Login();
 
     return (0);
