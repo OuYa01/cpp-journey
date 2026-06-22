5644f3878d5dc5c845f5ef3e3d3d68fc9c2bfd49
OuYa01 <Laamri2002@gmail.com>
2026-06-22 16:48:48 +0100

    Print detiles functions in atm sys

---
+++ projects/3_BankSysteme1/ATM_SYSTEME/Atm_print_detiles.cpp
@@ -0,0 +1,27 @@
+#include "Atm.h"
+
+
+
+void ShowLoginScreen()
+{
+    cout << "-------------------------------------------\n";
+    cout << "\t\t Login Screen\n";
+    cout << "-------------------------------------------\n";
+}
+
+
+void showAtmMenueScreen()
+{
+    cout << "===============================================\n";
+    cout << "\t\tATM Screen\n";
+    cout << "===============================================\n";
+    cout << "\t[1] Quick Withdraw.\n";
+    cout << "\t[2] Normal Withdraw\n";
+    cout << "\t[3] Deposit.\n";
+    cout << "\t[4] Check Balance.\n";
+    cout << "\t[5] Logout.\n";
+    cout << "===============================================\n";
+    cout << "Choose what do you want to do? [1 to 5]?  ";
+}
+
+
